e5e322eaefc9ac465f4f258d42385389b6ad8443
BNFLYD <68260203+BNFLYD@users.noreply.github.com>
2023-11-07 17:31:45 -0300

    Update hello.js

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,4 +1,4 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// ruta de prueba de la base de datos
 import { pool } from "../../../config/db";
 
 export default async function handler(req, res) {
